9357f33e12508563aae0cccd3b68a0b7bc8cedf0
yesmore <3224266014@qq.com>
2023-03-29 22:09:43 +0800

    chat api

--- api/chat.ts
+++ api/chat.ts
@@ -1,10 +1,30 @@
+import { generatePayload, parseOpenAIStream } from "../utils/openAI";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+// #vercel-disable-blocks
+// import { fetch, ProxyAgent } from "undici";
+// #vercel-end
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  console.log(req);
+const apiKey = process.env.OPENAI_API_KEY || "";
+const httpsProxy = process.env.HTTPS_PROXY || "";
+const baseUrl = process.env.OPENAI_API_BASE_URL || "";
 
-  return res.json({
-    message: `Hello ${name}!`,
-  });
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { messages } = req.body;
+  console.log(req.body);
+
+  if (!messages) {
+    return new Response("No input text");
+  }
+  const initOptions = generatePayload(apiKey, messages);
+
+  // // #vercel-disable-blocks
+  // if (httpsProxy) {
+  //   initOptions["dispatcher"] = new ProxyAgent(httpsProxy);
+  // }
+  // // #vercel-end
+
+  // @ts-ignore
+  const response = (await fetch( `${baseUrl}/v1/chat/completions`, initOptions)) as Response;
+
+  return new Response(parseOpenAIStream(response));
 }
---
+++ utils/openAI.ts
@@ -0,0 +1,70 @@
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
+import type { ChatMessage } from "../types";
+
+export const generatePayload = (
+  apiKey: string,
+  messages: ChatMessage[]
+): RequestInit => ({
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  },
+  method: "POST",
+  body: JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 0.6,
+    stream: true,
+  }),
+});
+
+export const generateProxyPayload = (messages: ChatMessage[]): RequestInit => ({
+  headers: {
+    "Content-Type": "application/json",
+  },
+  method: "POST",
+  body: JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 0.6,
+  }),
+});
+
+export const parseOpenAIStream = (rawResponse: Response) => {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const streamParser = (event: ParsedEvent | ReconnectInterval) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta?.content || "";
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
+        }
+      };
+
+      const parser = createParser(streamParser);
+      for await (const chunk of rawResponse.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+};
+
