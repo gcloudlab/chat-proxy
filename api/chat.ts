6910264e3cff2ad5a427a6ce529432523c09a321
yesmore <3224266014@qq.com>
2023-03-29 21:20:45 +0800

    test

--- api/chat.ts
+++ api/chat.ts
@@ -2,6 +2,8 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
+  console.log(req);
+
   return res.json({
     message: `Hello ${name}!`,
   });
