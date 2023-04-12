b6875f5d2990bfd5b1fef552713eff2a6d7cf19a
songjunxi <3224266014@qq.com>
2023-04-12 09:56:41 +0800

    add

---
+++ api/path.ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const paths = [
+    {
+      name: "QAchat",
+      path: "https://a.mydog.buzz",
+    },
+    {
+      name: "Pro",
+      path: "https://a.qachat.pro",
+    },
+    {
+      name: "导航",
+      path: "https://path.navnav.top",
+    },
+  ];
+
+  return res.send({ code: 200, data: paths, msg: "请求成功" });
+}
